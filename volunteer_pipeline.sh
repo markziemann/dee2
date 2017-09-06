e31ae3967e999564749956084e3cd9ebec1b8acc
markziemann <mark.ziemann@gmail.com>
2017-09-06 23:06:34 +1000

    fixing sftp key

--- volunteer_pipeline.sh
+++ volunteer_pipeline.sh
@@ -846,18 +846,17 @@ export -f myfunc
 count=1
 #while [ $count -lt 200 ] ; do
 while [ $count -lt 3 ] ; do
-    (( count++ ))
-    DIR=$(pwd)
-    echo "$count"
-    ACCESSION=$(myfunc $MY_ORG $ACC_REQUEST)
-    ( main "$ACCESSION" "$MY_ORG") && COMPLETE=1 || COMPLETE=0
+  (( count++ ))
+  DIR=$(pwd)
+  echo "$count"
+  ACCESSION=$(myfunc $MY_ORG $ACC_REQUEST)
+  ( main "$ACCESSION" "$MY_ORG") && COMPLETE=1 || COMPLETE=0
 
-    if [ "$COMPLETE" -eq "1" ] ; then
+  if [ "$COMPLETE" -eq "1" ] ; then
 
-      if [ ! -r ~/.ssh/guestuser ] ; then
-        mkdir ~/.ssh
+    mkdir ~/.ssh
 
-cat << EOF > ~/.ssh/guestuser
+    cat << EOF > ~/.ssh/guestuser
 -----BEGIN RSA PRIVATE KEY-----
 MIIEpAIBAAKCAQEAyLJ5TihXnb2ATexgYMIkzpHgopCbctWKH8rrPZNN6PALRYjg
 1ozfeMFylSvQilFw+6bCe7HlqUQ3e6pS/jHJukEyzbJOEVR4AwuZxxctI4QH00AL
@@ -887,15 +886,15 @@ Qzab+/WnlQMuslmCLxXXOijq5lEDJLJ0m9hZ0sdC+j13jsTCEOnyj/XJ3VgLKifP
 -----END RSA PRIVATE KEY-----
 EOF
 
-cat << EOF > ~/.ssh/guestuser.pub
+    cat << EOF > ~/.ssh/guestuser.pub
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDIsnlOKFedvYBN7GBgwiTOkeCikJty1Yofyus9k03o8AtFiODWjN94wXKVK9CKUXD7psJ7seWpRDd7qlL+Mcm6QTLNsk4RVHgDC5nHFy0jhAfTQAvZ4O9a+UQ4KGXE+DwSvlKM/OJRfDw6/ds0u8UdJDuqU1v+BsqEq/OXouTSfpjOX0L96LDNMqN8Qp9cBnnV+PIPZ+ZIVpWV6r63fO+JloZ+0W04sq0MD3BdeKixisG58R/OLG6NAXTgfO/4SDzXhuPOCRKlmIPJLvfk1TO7Q8Judc2NkDpCLKaKw6SHHQnvDeu+f+CaVgyZ4FrkZlmyed9EG+PSCtAh5+QtLfRH mdz@opti
 EOF
 
-chmod -R 700 ~/.ssh
-      fi
-      cd ../data/$MY_ORG
-      zip -r $ACCESSION.$MY_ORG.zip $ACCESSION
-      sftp -i ~/.ssh/guestuser guestuser@$SFTP_URL << EOF
+    chmod -R 700 ~/.ssh
+
+    cd ../data/$MY_ORG
+    zip -r $ACCESSION.$MY_ORG.zip $ACCESSION
+    sftp -i ~/.ssh/guestuser guestuser@$SFTP_URL << EOF
 put $ACCESSION.$MY_ORG.zip
 EOF
 
