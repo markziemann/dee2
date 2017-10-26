429683951d1ee3d4455563a3679211c88665c9b8
Mark Ziemann <mark.ziemann@monash.edu>
2017-10-26 23:55:42 +1100

    use library(elasticsearchr)

--- search_eng_data.R
+++ search_eng_data.R
@@ -1,38 +1,36 @@
 library(SRAdb)
-library(dplyr)
-library(readr)
-library(jsonlite)
+library("elasticsearchr")
+#library(dplyr)
+#library(readr)
+#library(jsonlite)
+#library(curl)
 
 setwd("../sradb")
-
 sqlfile <- 'SRAmetadb.sqlite'
 sra_con <- dbConnect(SQLite(),sqlfile)
-
-
 study<-dbGetQuery(sra_con,"select * from study limit 100000000")
 experiment<-dbGetQuery(sra_con,"select * from experiment limit 100000000")
 run<-dbGetQuery(sra_con,"select * from run limit 100000000")
 sample<-dbGetQuery(sra_con,"select * from sample limit 100000000")
 
-
-a<-merge(head(experiment),study,by="study_accession")
+#a<-merge(head(experiment),study,by="study_accession")
+a<-merge(experiment,study,by="study_accession")
 a<-merge(a,sample,by="sample_accession")
 a<-merge(a,run,by="experiment_accession")
 
+a<-a[,grep("\\.y$",names(a),invert=T)]
+a<-a[,grep("\\.1$",names(a),invert=T)] 
+names(a)=gsub("\\.x","",names(a))
 
-a %>% 
-    toJSON() %>%
-    write_lines("test.json")
+b<-head(a[which(a$scientific_name=="Saccharomyces cerevisiae"),])
 
+#match completed SRR numbers from the 
+
+elastic("http://localhost:9200", "bdata", "data") %index% b
 
 
 q()
-http://joelabrahamsson.com/elasticsearch-101/
-#need to write json to file then use curl to index
-sed 's/{/\n\{\n/g;s/\}/\n\}\n/g;s/","/",\n"/g' test.json  \
-| tr -s ' ' | tr -d '\n' | cut -c2- | rev | cut -c2- | rev | sed 's/},{/}\n{/g' | tr '.' ' ' \
-| while read line ; do
-  echo $line > tmp.json
-  curl -XPUT "http://localhost:9200/dat/dat/1" -d"$(cat tmp.json)"
-done
-
+#This is how to query
+#curl -XPOST "http://localhost:9200/_search" -d'{"query": {"query_string": {"query": "DRX000006" } }}'
+#http://joelabrahamsson.com/elasticsearch-101/
+#https://cran.r-project.org/web/packages/elasticsearchr/vignettes/quick_start.html
