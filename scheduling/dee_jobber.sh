ef439b869e88aec6c87f9b17d8a1665bbdbc5b97
markziemann <mark.ziemann@gmail.com>
2018-01-18 22:11:44 +1100

    Delete dee_jobber.sh

--- scheduling/dee_jobber.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ $(ps -aef | grep docker | grep -c tallyup ) -eq 0 ] ; then
-  echo "no dockers running, starting tallyup" >> test_jobber.txt
-  nohup docker run mziemann/tallyup
-else
-  echo "docker tallyup job already running - waiting..." >> test_jobber.txt
-fi
-
