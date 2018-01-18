316f02f4fc89200607cd15bad5020d3377368ade
Mark Ziemann <mark.ziemann@monash.edu>
2018-01-18 15:05:21 +1100

    keyfix

--- volunteer_pipeline.sh
+++ volunteer_pipeline.sh
@@ -1292,7 +1292,7 @@ touch /dee2/.ssh/known_hosts
 chmod +w /dee2/.ssh/known_hosts
 
 if [ -z $(ssh-keygen -F $SFTP_URL ) ]; then
-  ssh-keyscan -H $SFTP_URL >> /dee2/.ssh/known_hosts
+  ssh-keyscan -H $SFTP_URL -f /dee2/.ssh/known_hosts
 fi
 
 cat << EOF > /dee2/.ssh/guestuser
