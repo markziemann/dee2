0b2f18b54b2813c010c1dc454afdd7b9a6103b0d
Mark Ziemann <mark.ziemann@monash.edu>
2018-04-17 15:56:49 +1000

    working - but need efficient way to remove redundancy in bulk dump

--- dee_pipeline.R
+++ dee_pipeline.R
@@ -6,8 +6,8 @@ library(parallel)
 library(data.table)
 
 CORES=ceiling(detectCores()/2)
-for (org in c("ecoli" , "scerevisiae" ,  "rnorvegicus" , "athaliana", "celegans", "dmelanogaster", "drerio", "hsapiens", "mmusculus" ) ) {
-#org="ecoli"
+#for (org in c("ecoli" , "scerevisiae" , "celegans", "athaliana",  "rnorvegicus" , "celegans", "dmelanogaster", "drerio", "hsapiens", "mmusculus" ) ) {
+org="celegans"
   #create a list of species full names
   species_list<-c("'Arabidopsis thaliana'","'Caenorhabditis elegans'","'Drosophila melanogaster'","'Danio rerio'",
   "'Escherichia coli'","'Homo sapiens'", "'Mus musculus'", "'Rattus norvegicus'", "'Saccharomyces cerevisiae'")
@@ -15,6 +15,8 @@ for (org in c("ecoli" , "scerevisiae" ,  "rnorvegicus" , "athaliana", "celegans"
   names(species_list)<- c("athaliana", "celegans", "dmelanogaster", "drerio",
   "ecoli", "hsapiens", "mmusculus", "rnorvegicus", "scerevisiae")
   species_name<-species_list[[org]]
+
+  print(org)
 
   ###Set some directories
   CODEWD=getwd()
@@ -68,269 +70,24 @@ for (org in c("ecoli" , "scerevisiae" ,  "rnorvegicus" , "athaliana", "celegans"
 ########################
 # Now determine which datasets have already been processed and completed
 ########################
-  setwd(DATAWD)
-  finished_files<-list.files(path = ".", pattern = "finished" , full.names = FALSE, recursive = TRUE, no.. = FALSE)
+  setwd(CODEWD)
+  finished_files<-list.files(path = DATAWD, pattern = "finished" , full.names = FALSE, recursive = TRUE, no.. = FALSE)
 
   if ( length(finished_files) > 0 ) { 
+   system(paste("./dee_pipeline.sh",org))
+   runs_done<-unique( read.table(paste(DATAWD,"/",org,"_val_list.txt",sep=""),stringsAsFactors=F)[,1] )
+   print(paste(length(runs_done),"new runs completed"))
+   runs_todo<-base::setdiff(runs, runs_done)
+   print(paste(length(runs_todo),"requeued runs"))
 
-   runs_done<-basename(as.character(strsplit(finished_files,".finished")))
-   runs_todo<-setdiff(runs, runs_done)
-   setwd(CODEWD)
+   #Update queue on webserver
    queue_name=paste(QUEUEWD,"/",org,".queue.txt",sep="")
-
-   setwd(DATAWD)
-
-   #define read.tsv
-   read.tsv<-function(file, header = TRUE, sep = "\t", quote = "\"", dec = ".", fill = TRUE, comment.char = "", ...){
-   read.table(file = file, header = header, sep = sep, quote = quote, dec = dec, fill = fill, row.names=1, comment.char = comment.char, ...)}
-
-   print("generate a list of se tsv files to import and merge")
-   se_list<-list.files(path = ".", pattern = "\\.se.tsv$" , full.names = TRUE , recursive = TRUE, no.. = FALSE)
-
-   #rowcnt<-function(file){nrow(fread(file,fill=T)) }
-   rowcnt<-function(file){nrow(read.table(file,fill=T)) }
-
-   #add the failed datasets to the todo list
-   LEN=length(rownames(subset(file.info(se_list),size==0)))
-   if (LEN!=0){
-    p<-rownames(subset(file.info(se_list),size==0))
-    p<-unlist(strsplit(rownames(p),'/'))
-    p<-p[seq(1,length(p),2)]
-    runs_todo<-unique(union(runs_todo,p))
-   }
-
-   se_list<-rownames(subset(file.info(se_list),size!=0))
-
-  #Need to ensure matrix will be square
-   x<-as.matrix(table(factor(as.numeric(mclapply(se_list,rowcnt, mc.cores = CORES )))))
-   expected_len=as.numeric(rownames(tail(x,n=1)))
-   y<-t(as.data.frame(mclapply(se_list,rowcnt, mc.cores = CORES  )))
-   rownames(y)=as.character(se_list)
- 
-   LEN=length(rownames(subset(y,y!=expected_len)))
-   if (LEN!=0){
-    p<-rownames(subset(y,y!=expected_len))
-    p<-unlist(strsplit(p,'/'))
-    p<-p[seq(1,length(p),2)]
-    runs_todo<-unique(union(runs_todo,p))
-   }
-
-   se_list<-rownames(subset(y,y=!expected_len))
-   se_name=paste(org,"_se_list.txt",sep="")
-   write.table(se_list,file=se_name,quote=F)
- 
-############################
-#  Matrix generation is simply too memory hungry and slow not doing for now
-#############################
-
-   print("se list finished OK")
-
-   #now focus on kallisto data
-  
-   ke_list<-list.files(path = ".", pattern = "\\.ke.tsv$" , full.names = TRUE , recursive = TRUE, no.. = FALSE)
-
-   LEN=length(rownames(subset(file.info(se_list),size==0)))
-   if (LEN!=0){
-    p<-rownames(subset(file.info(ke_list),size==0))
-    p<-unlist(strsplit(rownames(subset(p,p!=expected_len)),'/'))
-    p<-p[seq(1,length(p),2)]
-    runs_todo<-unique(union(runs_todo,p))
-   }
-
-   ke_list<-rownames(subset(file.info(ke_list),size!=0))
-   x<-as.matrix(table(factor(as.numeric(mclapply(ke_list,rowcnt , mc.cores = CORES )))))
-   expected_len=as.numeric(rownames(tail(x,n=1)))
-
-   y<-t(as.data.frame(mclapply(ke_list,rowcnt, mc.cores = CORES  )))
-   rownames(y)=as.character(ke_list)
-
-   LEN=length(rownames(subset(y,y!=expected_len)))
-   if (LEN!=0){
-    p<-rownames(subset(y,y!=expected_len))
-    p<-unlist(strsplit(p,'/'))
-    p<-p[seq(1,length(p),2)]
-    runs_todo<-unique(union(runs_todo,p))
-   }
-
-   y<-t(as.data.frame(mclapply(ke_list,rowcnt , mc.cores = CORES )))
-
-   rownames(y)=as.character(ke_list)
-   ke_list<-rownames(subset(y,y==expected_len))
-   ke_name=paste(org,"_ke_list.txt",sep="")
-   write.table(ke_list,file=ke_name,quote=F)
-
-############################
-#  Matrix generation is simply too memory hungry and slow not doing for now
-############################
-   print("ke list finished OK")
-
-   #QC data
-   #create the qc matrix
-   system("for I in `find . | grep qc$` ; do grep ':' $I > tmp ; mv tmp $I ; done 2>/dev/null ")
-   print("generate a list of qc files to import and merge")
-   qc_list<-list.files(path = ".", pattern = "\\.qc$" , full.names = TRUE , recursive = TRUE, no.. = FALSE)
-
-  # rowcnt<-function(file){nrow(fread(file,fill=T)) }
-
-   #add the failed datasets to the todo list
-   LEN=length(rownames(subset(file.info(qc_list),size==0)))
-   if (LEN!=0){
-    p<-rownames(subset(file.info(qc_list),size==0))
-    p<-strsplit(p,'/')
-    p<-sapply(p, "[[", 2)
-    runs_todo<-unique(union(runs_todo,p))
-   }
-
-   qc_list<-rownames(subset(file.info(qc_list),size!=0))
-
-   #Need to ensure matrix will be square
-   x<-as.matrix(table(factor(as.numeric(mclapply(qc_list,rowcnt, mc.cores = CORES  )))))
-   expected_len=as.numeric(rownames(tail(x,n=1)))
-   y<-t(as.data.frame(mclapply(qc_list,rowcnt, mc.cores = CORES  )))
-   rownames(y)=as.character(qc_list)
-
-   LEN=length(rownames(subset(y,y!=expected_len)))
-   if (LEN!=0){
-    p<-rownames(subset(y,y!=expected_len))
-    p<-unlist(strsplit(p,'/'))
-    p<-p[seq(1,length(p),2)]
-    runs_todo<-unique(union(runs_todo,p))
-   }
-
-   qc_list<-rownames(subset(y,y==expected_len))
-   qc_name=paste(org,"_qc_list.txt",sep="")
-   write.table(qc_list,file=qc_name,quote=F)
-
-   #redefine read.colon.tsv
-   read.colon.tsv<-function(file, header = FALSE, sep = ":", quote = "\"", dec = ".", fill = TRUE, comment.char = "", ...){
-   read.table(file = file, sep = sep, quote = quote, dec = dec, fill = fill, row.names=1, comment.char = comment.char, ...)}
-
-   #bringing in some QC data
-   qc<-do.call("cbind", mclapply(qc_list, read.colon.tsv,mc.cores = CORES ))
-   colnames(qc)<-sapply(strsplit(sub("./","",qc_list), "/"), head, 1)
-   print("qc list finished OK")
-
-   #need to do some logic to pass fail data based on QC info and fill in the field
-   #for col in numcols do blah blah
-   #if then if then
-   #qc$COL[29]=PASS
-   #qc$COL[29]=FAIL:FASTQ_LENGTH
-
-   #Moved this part to the end due to a small number of files posessing the wrong number of lines 
    write.table(runs_todo,queue_name,quote=F,row.names=F,col.names=F)
    SCP_COMMAND=paste("scp -i ~/.ssh/cloud/id_rsa ",queue_name ," ubuntu@118.138.240.228:~/Public")
    system(SCP_COMMAND)
 
-   setwd(CODEWD)
- 
-   #PASTE_MX_CMD=paste("./pastemx.sh",org)
-   #system(PASTE_MX_CMD)
-
-   #new section to obatin and update webserver metadata for completed runs
-   #start with accession information
-   runs_done<-setdiff(runs_done,runs_todo)
-
- if (length(runs_done)>0) {
-
-   #TODO: runs done .finished files renamed to .validated
-   #TODO: runs done become write only
-   se=NULL
-   ke=NULL
-   qc=NULL
-
-   se_func<-function(file){
-    y<-read.table(file)
-    y$run=strsplit(file,"/")[[1]][7]
-    y$gene=rownames(y)
-    rownames(y)=NULL
-    colnames(y)=c("count","run","gene")
-    y2<-y[,c(2,3,1)]
-    return(y2)
-  }
-
-  message("rbinding new se data")
-  se.new<-mclapply(paste("/scratch/mziemann/dee2/data/",org,"/",runs_done,"/",runs_done,".se.tsv",sep=""),se_func, mc.cores = CORES)
-  se.new<-rbindlist(se.new)
-
-  se_name=paste("/scratch/mziemann/dee2/mx/",org,"_se.tsv",sep="")
-  if(file.exists(se_name)) {
-   se<-read.table(se_name,header=T)
-   se<-rbind(se,se.new)
-  } else {
-   se<-se.new
-  }
-  write.table(unique(se),file=se_name,quote=F,row.names=F)
-  system(paste("pbzip2 -fk " ,se_name))
-
-#  message("appending new se data to db")
-#  con <- dbConnect(RSQLite::SQLite(), dbname = paste(org,"_se",sep="") )
-#  dbWriteTable(con, paste(org,"_se",sep="") , se, append = TRUE)
-
-  message("rbinding new ke data")
-  ke_func<-function(file){
-    y<-read.table(file,header=T)
-    y$run=strsplit(file,"/")[[1]][7]
-    y2<-y[,c(6,1,4)]
-    colnames(y2)=c("run","gene","count")
-    return(y2)
-  }
-
-  ke.new<-mclapply(paste("/scratch/mziemann/dee2/data/",org,"/",runs_done,"/",runs_done,".ke.tsv",sep=""),ke_func, mc.cores = CORES)
-  ke.new<-rbindlist(ke.new)
-
-  ke_name=paste("/scratch/mziemann/dee2/mx/",org,"_ke.tsv",sep="")
-  if(file.exists(ke_name)) {
-   ke<-read.table(ke_name,header=T)
-   ke<-rbind(ke,ke.new)
-  } else {
-   ke<-ke.new
-  }
-  write.table(unique(ke),file=ke_name,quote=F,row.names=F)
-  system(paste("pbzip2 -fk " ,ke_name))
-
-#  message("appending new ke data to db")
-#  con <- dbConnect(RSQLite::SQLite(), dbname = paste(org,"_ke",sep="") )
-#  dbWriteTable(con, paste(org,"_ke",sep="") , ke, append = TRUE)
-
-  message("rbinding new qc data")
-  qc_func<-function(file){
-    y<-read.colon.tsv(file)
-    y$run=strsplit(file,"/")[[1]][7]
-    y$metric=rownames(y)
-    rownames(y)=NULL
-    y2<-y[,c(2,1,3)]
-    colnames(y2)=c("run","metric","count")
-    return(y2)
-  }
-
-  qc.new<-mclapply(paste("/scratch/mziemann/dee2/data/",org,"/",runs_done,"/",runs_done,".qc",sep=""),qc_func, mc.cores = CORES)
-  qc.new<-rbindlist(qc.new)
-
-  qc_name=paste("/scratch/mziemann/dee2/mx/",org,"_qc.tsv",sep="")
-  if(file.exists(qc_name)) {
-   qc<-read.table(qc_name,header=T,fill=T)
-   qc<-rbind(qc,qc.new,fill=T)
-  } else {
-   qc<-qc.new
-  }
-  write.table(unique(qc),file=qc_name,quote=F,row.names=F)
-  system(paste("pbzip2 -fk " ,qc_name))
-
-#  message("appending new qc data to db")
-#  con <- dbConnect(RSQLite::SQLite(), dbname = paste(org,"_qc",sep="") )
-#  dbWriteTable(con, paste(org,"_qc",sep="") , qc, append = TRUE)
-
-  for (run in runs_done) {
-    FIN=paste("/scratch/mziemann/dee2/data/",org,"/",run,"/",run,".finished",sep="")
-    VAL=paste("/scratch/mziemann/dee2/data/",org,"/",run,"/",run,".validated",sep="")
-    DIR=paste("/scratch/mziemann/dee2/data/",org,"/",run,sep="")
-
-    file.rename(FIN,VAL)
-    system(paste("chmod 0544 ",DIR) )
-   }
-
+   #Update metadata on webserver
    accessions_done<-accessions[which(accessions$run %in% runs_done),]
- 
    write.table(accessions_done,file=paste(SRADBWD,"/",org,"_accessions.tsv",sep=""),quote=F,row.names=F)
 
    #now the metadata   experiment submission     study    sample        run
@@ -404,7 +161,6 @@ for (org in c("ecoli" , "scerevisiae" ,  "rnorvegicus" , "athaliana", "celegans"
    system(SCP_COMMAND)
 
   }
-  }
 
   setwd("/scratch/mziemann/dee2/code/")
 
@@ -443,4 +199,4 @@ for (org in c("ecoli" , "scerevisiae" ,  "rnorvegicus" , "athaliana", "celegans"
   dev.off()
   system("scp -i ~/.ssh/cloud/cloud2.key dee_datasets.png ubuntu@118.138.240.228:/mnt/dee2_data/mx")
 
-}
+#}
