304c19aac217a82888f2b2228b1ac4f2cd7387a1
markziemann <mark.ziemann@gmail.com>
2018-11-14 16:01:01 +1100

    prioritising gap filling on 4 nodes

--- misc/gap_fill.sh
+++ misc/gap_fill.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
->gap_fill.txt
+
 for ORG in athaliana celegans dmelanogaster drerio ecoli hsapiens mmusculus rnorvegicus scerevisiae ; do
   MDATA=../sradb/${ORG}_metadata.tsv.cut
   DATA=../data/${ORG}
@@ -15,6 +15,31 @@ for ORG in athaliana celegans dmelanogaster drerio ecoli hsapiens mmusculus rnor
     done
     echo $SRP $TOT $DONE $NOT_DONE
   done | awk '$3/$2<1 && $3/$2>.5 {print $1,$2,$3,$3/$2,$4}' | sort -k5g \
-  |cut -d ' ' -f5 | tr -d  '\n' |sed 's/$/\n/' | sed "s/^/${ORG} /" | sed 's/,//'
-done | tee gap_fill.txt
+  |cut -d ' ' -f5 | tr ',' '\n' | sed '/^$/d' > ${ORG}.queue.txt
+
+  LENGTH=$(wc -l <  ${ORG}.queue.txt )
+  if [ $LENGTH -gt 4000 ] ;  then
+    awk '{print $0, $0}' ${ORG}.queue.txt | cut -c4- | sort -k1gr | awk '{print $2}' > tmp
+    mv tmp ${ORG}.queue.txt
+    split -n4 ${ORG}.queue.txt ${ORG}.queue.txt.
+    scp ${ORG}.queue.txt.aa mdz@10.115.170.32:/home/mdz/bfx/dee2/gap_fill
+    scp -i ~/.ssh/monash/id_rsa ${ORG}.queue.txt.ab mziemann@118.138.234.73:/home/mziemann/dee2/gap_fill/
+    scp -i ~/.ssh/monash/id_rsa ${ORG}.queue.txt.ac mziemann@118.138.234.56:/home/mziemann/dee2/gap_fill/
+    scp -i ~/.ssh/monash/id_rsa ${ORG}.queue.txt.ad mziemann@118.138.246.227:/scratch/mziemann/dee2/gap_fill/
+  fi
+done
+
+exit
+###
+# Run this on the other machine
+#!/bin/bash
+ORG=$(echo $0 | sed 's@./gap_fill_@@' | sed 's@.sh$@@')
+ABBREV=$(echo $ORG | cut -c-3)
+#check path to queue
+for FILE in /scratch/mziemann/dee2/gap_fill/${ORG}.queue.txt.* ; do
+  for SRR in $(shuf $FILE ) ; do
+    echo $SRR
+    docker run mziemann/tallyup_${ABBREV} $ORG $SRR && sed -i "/^${SRR}/d" $FILE
+  done
+done
 
