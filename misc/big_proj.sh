0622c14a9d4ba1941e8c2e9d88880c7d5bce0b62
markziemann <mark.ziemann@gmail.com>
2019-01-09 15:21:36 +1100

    updating the big project script

--- misc/big_proj.sh
+++ misc/big_proj.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-## Package large datasets for public use
+echo need to increase ULIMIT for this
+#sudo sh -c "ulimit -n 1000000 && exec su $LOGNAME"
 
-for ORG in athaliana celegans dmelanogaster drerio ecoli hsapiens mmusculus rnorvegicus scerevisiae ; do
+#for ORG in athaliana celegans dmelanogaster drerio ecoli hsapiens mmusculus rnorvegicus scerevisiae ; do
+for ORG in athaliana celegans dmelanogaster drerio ecoli rnorvegicus scerevisiae ; do
 
 FULL_METADATA=${ORG}_metadata.tsv.cut
 DEE2_ACCESSIONS=${ORG}_accessions.tsv
@@ -22,6 +24,20 @@ cut -d ' ' -f2 $DEE2_ACCESSIONS \
     SRP_DIR=$DIR/${SRP}_${GSE}
     if [ ! -r $SRP_DIR.zip ] ; then
       mkdir -p $SRP_DIR
+
+      #package the gene and tx info
+      PFX=$(echo $ORG | cut -c-3)
+      cp ../gene_info/${PFX}_gene_info.tsv  $SRP_DIR/GeneInfo.tsv
+      cp ../gene_info/${PFX}_tx_info.tsv $SRP_DIR/TxInfo.tsv
+
+      #package the metadata summary
+      head -1 $FULL_METADATA > $SRP_DIR/MetadataSummary.tsv
+      grep -w $SRP $DEE2_ACCESSIONS | cut -d ' ' -f4 | grep -wFf - $FULL_METADATA >> $SRP_DIR/MetadataSummary.tsv
+
+      #package the metadata in full
+      COMPLETE_METADATA=${ORG}_metadata.tsv
+      head -1 $COMPLETE_METADATA > $SRP_DIR/MetadataFull.tsv
+      grep -w $SRP $DEE2_ACCESSIONS | cut -d ' ' -f4 | grep -wFf - $COMPLETE_METADATA >> $SRP_DIR/MetadataFull.tsv
 
       #package the gene counts
       SRRLIST=$(grep -w $SRP $DEE2_ACCESSIONS | cut -d ' ' -f4 \
