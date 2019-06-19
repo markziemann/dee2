f791a196969a6c0d7ffe7b047c83b2568a8070f4
markziemann <mark.ziemann@gmail.com>
2019-06-19 23:17:18 +1000

    cut out a lot of unnecessary code to make it quicker

--- frontend/cgibin/request2.sh
+++ frontend/cgibin/request2.sh
@@ -115,22 +115,9 @@ QS2=`echo $QS | tr ' ' '\n' | cut -d '_' -f1 | tr '\n' ' '`
 PFX=$(echo $ORG | cut -c-3)
 
 #################################################
-# Accession numbers
-#################################################
-head -1 $ACCESSIONS > $USRDIR/MetadataSummary.tsv
-echo $QS2 | tr ' ' '\n' | grep -wFf - $ACCESSIONS | sort >> $USRDIR/MetadataSummary.tsv
-
-#################################################
-# Accession numbers
-#################################################
-head -1 $METADATA > $USRDIR/MetadataFull.tsv
-echo $QS2 | tr ' ' '\n' | grep -wFf - $METADATA | sort >> $USRDIR/MetadataFull.tsv
-
-#################################################
 # Gene info - names and length
 #################################################
 cp ${PFX}_gene_info.tsv $USRDIR/GeneInfo.tsv
-cp ${PFX}_tx_info.tsv $USRDIR/TxInfo.tsv
 
 #################################################
 # STAR Gene counts
@@ -140,30 +127,6 @@ GENECOUNTS=$(echo $QS2 | tr ' ' '\n' | awk '{print $1"/"$1"_gene.cnt"}' | tr '\n
 cut -f-2 $USRDIR/GeneInfo.tsv | tr '\t' '_' \
 | paste - $GENECOUNTS > $USRDIR/GeneCountMatrix.tsv
 
-#################################################
-# QC matrix
-#################################################
-QCS=$(echo $QS2 | tr ' ' '\n' | awk '{print $1"/"$1".qcl"}' | tr '\n' ' ' | sed 's/$/\n/')
-ROWNAMES_QC=rownames_qc.txt
-paste $ROWNAMES_QC $QCS | cat <(echo SeqMetric2 $QS2 | tr ' ' '\t') - > $USRDIR/QC_Matrix.tsv
-
-#################################################
-# Kallisto Transcript counts
-#################################################
-TXCOUNTS=$(echo $QS2 | tr ' ' '\n' | awk '{print $1"/"$1"_tx.cnt"}' | tr '\n' ' ' | sed 's/$/\n/')
-ROWNAMES_TX=rownames_tx.txt
-paste $ROWNAMES_TX $TXCOUNTS > $USRDIR/TxCountMatrix.tsv
-
-#################################################
-# README file based on contents
-#################################################
-cp /usr/lib/cgi-bin/contents.md $USRDIR/README.md
-
-#################################################
-# Collect run logs
-#################################################
-# Collect reports
-LOGS=$(echo $QS2 | tr ' ' '\n' | awk '{print $1"/"$1".log"}' | tr '\n' ' ' | sed 's/$/\n/')
 cp $LOGS $LOGDIR
 cd $USRDIR
 
@@ -174,6 +137,7 @@ curl -b cookies.txt -v -L 'http://degust.erc.monash.edu/upload' -F "authenticity
 | grep -v 'front-loader.gif'
 
 URL=$(grep -m1 'http://degust.erc.monash.edu/degust/compare.html?code=' tmp | cut -d ' ' -f3)
+echo "Sending your GeneCountMatrix.tsv to Degust."
 echo "Here is your Degust <a href=$URL target=\"_blank\"> link</a>. It will open a new tab."
 echo "<br><br>"
 echo '<FORM><INPUT Type="button" VALUE="Go back to DEE2" onClick="history.go(-1);return true;" style="font-size : 22px;" ></FORM>'
