e7bfdb5e9b5aa3d05e4cd9ebbe43bde5e6cae7a3
markziemann <mark.ziemann@gmail.com>
2021-10-21 22:29:26 +1100

    downloader

--- pipeline/downloader.sh
+++ pipeline/downloader.sh
@@ -2,14 +2,50 @@
 
 ORG=$1
 
+if [ -z $ORG ] ; then
+
+  echo "Error: ORG not set!"
+  exit
+
+fi
+
+FILECNT=$(ls ${ORG}_*.sra | wc -l)
+
 while true ; do
 
-  SRR=$(curl "http://dee2.io/cgi-bin/acc.sh?ORG=${ORG}&submit" \
-  | grep ACCESSION \
-  | cut -d '=' -f2 )
+  if [ $FILECNT -lt 100 ] ; then
 
-  echo $SRR
+    SRR=$(curl "http://dee2.io/cgi-bin/acc.sh?ORG=${ORG}&submit" \
+    | grep ACCESSION \
+    | cut -d '=' -f2 )
 
-  prefetch -X 9999999999999 -o ${ORG}_${SRR}.sra $SRR
+    echo $SRR
 
+    prefetch -X 9999999999999 -o ${ORG}_${SRR}.sra $SRR
+
+    FILECNT=$(ls ${ORG}_*.sra | wc -l)
+
+  else
+
+    sleep 10
+
+  fi
+
+  ZIPCNT=$(ls *${ORG}.zip | grep -c zip$ )
+
+  if [ $ZIPCNT -gt 0 ] ; then
+
+    SFTP_URL=$(curl dee2.io/ip)
+
+    for ZIP in $(ls *${ORG}.zip) ; do
+
+      sftp -i ~/.ssh/guestuser guestuser@$SFTP_URL << EOF
+put $ZIP
+EOF
+
+      rm -f $ZIP
+
+    done
+  fi
 done
+
