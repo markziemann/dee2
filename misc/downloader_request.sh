bc0ba9630b7a73e8b0ee08ab1e86a4da51b93406
markziemann <mark.ziemann@gmail.com>
2022-07-06 21:55:11 +1000

    downloader request

---
+++ misc/downloader_request.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+ORG=$1
+SRRLIST=$2
+TXOUT=TxCountMatrix.tsv
+GOUT=GeneCountMatrix.tsv
+
+if [ -z $ORG ] ; then
+
+  echo "Error: ORG not set!"
+  exit
+
+fi
+
+for SRR in $(cat $SRRLIST) ; do
+
+    echo $SRR
+
+    prefetch -X 9999999999999 -o ${ORG}_${SRR}.sra $SRR
+
+done
+
+docker run -v $(pwd):/dee2/mnt mziemann/tallyup hsapiens -d
+
+for ZIP in $(ls *${ORG}.zip) ; do
+
+  scp -i ~/.ssh/dee2 $ZIP ubuntu@dee2.io:~/upload
+
+done
+
+for ZIP in *zip ; do unzip $ZIP ; done
+
+rm $(find . | grep tmp)
+
+for KE in $(find . | grep ke) ; do
+  cut -f4 $KE | sed 's/_est_counts//' > $KE.tmp
+done
+
+FILE1=$(find . | grep ke | head -1)
+echo Tx_ID > $TXOUT
+cut -f1 $FILE1 | sed 1d > $TXOUT
+paste $TXOUT $(find . | grep ke.tsv.tmp) > tmp && mv tmp $TXOUT
+
+for SE in $(find . | grep se) ; do
+  cut -f2 $SE > $SE.tmp
+done
+
+FILE1=$(find . | grep se | head -1)
+echo Gene_ID > $GOUT
+cut -f1 $FILE1 | sed 1d > $GOUT
+paste $GOUT $(find . | grep ke.tsv.tmp) > tmp && mv tmp $GOUT
+
+mkdir data
+cp -r $SRRLIST SRR* GeneCountMatrix.tsv TxCountMatrix.tsv data
+rm -rf $(find data | grep zip)
+zip -r data.zip data
