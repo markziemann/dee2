f3331aeec0eae36f45b3f5b79422a3a141e1b049
markziemann <mark.ziemann@gmail.com>
2022-07-13 10:25:09 +1000

    fixed bug with request script

--- misc/downloader_request.sh
+++ misc/downloader_request.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#run like this
+#bash downloader_request.sh hsapiens sanjid.txt
+
 ORG=$1
 SRRLIST=$2
 TXOUT=TxCountMatrix.tsv
@@ -24,6 +27,7 @@ docker run -v $(pwd):/dee2/mnt mziemann/tallyup hsapiens -d
 
 for ZIP in $(ls *${ORG}.zip) ; do
 
+  echo $ZIP
   scp -i ~/.ssh/dee2 $ZIP ubuntu@dee2.io:~/upload
 
 done
@@ -36,19 +40,19 @@ for KE in $(find . | grep ke) ; do
   cut -f4 $KE | sed 's/_est_counts//' > $KE.tmp
 done
 
-FILE1=$(find . | grep ke | head -1)
+FILE1=$(find . | grep ke.tsv$ | head -1)
 echo Tx_ID > $TXOUT
-cut -f1 $FILE1 | sed 1d > $TXOUT
+cut -f1 $FILE1 > $TXOUT
 paste $TXOUT $(find . | grep ke.tsv.tmp) > tmp && mv tmp $TXOUT
 
 for SE in $(find . | grep se) ; do
   cut -f2 $SE > $SE.tmp
 done
 
-FILE1=$(find . | grep se | head -1)
+FILE1=$(find . | grep se.tsv$ | head -1)
 echo Gene_ID > $GOUT
-cut -f1 $FILE1 | sed 1d > $GOUT
-paste $GOUT $(find . | grep ke.tsv.tmp) > tmp && mv tmp $GOUT
+cut -f1 $FILE1 > $GOUT
+paste $GOUT $(find . | grep se.tsv.tmp) > tmp && mv tmp $GOUT
 
 mkdir data
 cp -r $SRRLIST SRR* GeneCountMatrix.tsv TxCountMatrix.tsv data
