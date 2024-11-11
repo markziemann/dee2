467761e59e8d741a16265ba90e1e922d60f4983d
markziemann <mark.ziemann@gmail.com>
2024-11-12 08:58:41 +1100

    remove ascp

--- pipeline/volunteer_pipeline.sh
+++ pipeline/volunteer_pipeline.sh
@@ -410,32 +410,7 @@ if [ $MODE != 'FASTQ' ] ; then
   fi
 
   if [ ! -f $SRR.sra ] ; then
-    #build URL
-    BASEURL=ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByRun/sra
-    PFX1=$(echo $SRR | cut -c-3)
-    PFX2=$(echo $SRR | cut -c-6)
-    URL=anonftp@ftp.ncbi.nlm.nih.gov:sra/sra-instant/reads/ByRun/sra/${PFX1}/${PFX2}/${SRR}/${SRR}.sra
-    ID=$DEE_DIR/.ascp/aspera-license
-
-    if [ ! -d $DEE_DIR/.ascp ] ; then
-      mkdir -p $DEE_DIR/.ascp
-      cat << EOF > $ID
------BEGIN DSA PRIVATE KEY-----
-MIIBuwIBAAKBgQDkKQHD6m4yIxgjsey6Pny46acZXERsJHy54p/BqXIyYkVOAkEp
-KgvT3qTTNmykWWw4ovOP1+Di1c/2FpYcllcTphkWcS8lA7j012mUEecXavXjPPG0
-i3t5vtB8xLy33kQ3e9v9/Lwh0xcRfua0d5UfFwopBIAXvJAr3B6raps8+QIVALws
-yeqsx3EolCaCVXJf+61ceJppAoGAPoPtEP4yzHG2XtcxCfXab4u9zE6wPz4ePJt0
-UTn3fUvnQmJT7i0KVCRr3g2H2OZMWF12y0jUq8QBuZ2so3CHee7W1VmAdbN7Fxc+
-cyV9nE6zURqAaPyt2bE+rgM1pP6LQUYxgD3xKdv1ZG+kDIDEf6U3onjcKbmA6ckx
-T6GavoACgYEAobapDv5p2foH+cG5K07sIFD9r0RD7uKJnlqjYAXzFc8U76wXKgu6
-WXup2ac0Co+RnZp7Hsa9G+E+iJ6poI9pOR08XTdPly4yDULNST4PwlfrbSFT9FVh
-zkWfpOvAUc8fkQAhZqv/PE6VhFQ8w03Z8GpqXx7b3NvBR+EfIx368KoCFEyfl0vH
-Ta7g6mGwIMXrdTQQ8fZs
------END DSA PRIVATE KEY-----
-EOF
-      chmod 700 $DEE_DIR/.ascp
-    fi
-    prefetch -X 9999999999999 -a "/usr/local/bin/ascp|/dee2/.ascp/aspera-license" $SRR \
+    prefetch -X 9999999999999 $SRR \
     || ( echo $SRR failed download with prefetch | tee -a $SRR.log ; sleep 5 ; exit1 ; return 1 )
     mv /dee2/ncbi/public/sra/${SRR}.sra .
   fi
